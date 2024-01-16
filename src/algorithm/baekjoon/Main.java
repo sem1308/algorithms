00e258dfbb8e005e776e5f8353ada042aea29164
한수한 <hshhan0221@naver.com>
2024-01-16 13:55:30 +0900

    solve problem in baekjoon(14890)

---
+++ src/algorithm/baekjoon/Main.java
@@ -0,0 +1,14 @@
+package algorithm.baekjoon;
+
+import java.util.*;
+import java.io.*;
+
+public class Main{
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+	}
+
+}
