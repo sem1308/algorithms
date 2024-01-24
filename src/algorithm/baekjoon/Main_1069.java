d368314ff9d62c2403919872ba63cf3902163065
한수한 <hshhan0221@naver.com>
2024-01-24 17:57:26 +0900

    add sub code

--- src/algorithm/baekjoon/Main_1069.java
+++ src/algorithm/baekjoon/Main_1069.java
@@ -9,38 +9,29 @@ public class Main_1069 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(br.readLine());
         
-        int X,Y,D,T;
-        
-        X = Integer.parseInt(tokenizer.nextToken());
-        Y = Integer.parseInt(tokenizer.nextToken());
-        D = Integer.parseInt(tokenizer.nextToken());
-        T = Integer.parseInt(tokenizer.nextToken());
-        
-        /*
-			1. 걸어서 갈 수 있는 시간(거리) (dist) 구함
-			2. 점프를 n번 해서 갈 수 있는 최소 거리 중 최솟값 구함
-			    - n*D >= dist --> n*T
-			    - n*D < dist --> n*T + (dist - n*D)
-			       - 최대 n = dist / D
-        */
-        
-        double dist = Math.sqrt(Math.pow(X, 2)+Math.pow(Y, 2));
+        int[] P = new int[N];
 
-        int n = (int)(dist / D);
+        int minP = 50;
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+			P[i] = Integer.parseInt(tokenizer.nextToken());
+			Math.min(minP, P[i]);
+		}
         
-        double t1, t2;
-        if(n == 0) {
-            t1 = 2*T;
-            t2 = T + D - dist;
-        }else {
-            t1 = (n+1)*T;
-            t2 = n*(T-D) + dist;
-        }
+        int M = Integer.parseInt(br.readLine());
         
-        double answer = Math.min(dist, Math.min(t1,t2));
+        // dp[m][n] : m원으로 만들 수 있는 최대 n자리 숫자
+        // dp[m][n] = max(p[i] * 10^n + dp[m-p[i]][n-1])
         
-        System.out.println(answer);
+        String[][] dp = new String[M+1][M+1];
+        
+        for (int i = 1; i <= M; i++) {
+			for (int j = 1; j <= M; j++) {
+				
+			}
+		}
+        
     }
 }
