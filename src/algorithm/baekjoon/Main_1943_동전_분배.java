e2be902bf55d8cc57bf2bf3f17003e58318a5710
한수한 <hshhan0221@naver.com>
2024-03-13 17:58:52 +0900

    add codes

--- src/algorithm/baekjoon/Main_1943_동전_분배.java
+++ src/algorithm/baekjoon/Main_1943_동전_분배.java
@@ -3,50 +3,107 @@ package algorithm.baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main_1943_동전_분배{
 	
+	static class Coin implements Comparable<Coin>{
+		int num;
+		int cnt;
+		
+		public Coin(int num, int cnt) {
+			this.num = num;
+			this.cnt = cnt;
+		}
+
+		@Override
+		public int compareTo(Coin o) {
+			// TODO Auto-generated method stub
+			return this.num - o.num;
+		}
+	}
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		for (int i = 0; i < 3; i++) {
 			int N = Integer.parseInt(br.readLine());
 			
-			int[] nums = new int[N];
-			int[] cnts = new int[N];
+			Coin[] coins = new Coin[N];
 			
 			int sum = 0;
 			for (int j = 0; j < N; j++) {
 				StringTokenizer tokens = new StringTokenizer(br.readLine());
 				
-				nums[i] = Integer.parseInt(tokens.nextToken());
-				cnts[i] = Integer.parseInt(tokens.nextToken());				
-				sum += nums[i] * cnts[i];
-			}
+				int num = Integer.parseInt(tokens.nextToken());
+				int cnt = Integer.parseInt(tokens.nextToken());	
 
-			/*
-			 * �� ���� ���忡�� ����
-			 * A, B�� ������ �����شٸ�
-			 * A�� ���ؼ��� ����
+				coins[j] = new Coin(num, cnt);
+				
+				sum += num * cnt;
+			}
+			
+			if(sum % 2 == 1) {
+				System.out.println(0);
+				continue;
+			}
+			
+			int halfSum = sum / 2;
+
+			/* 
+			 * 7 7 7
+			 * 5 5 5
+			 * 3 3
 			 * 
-			 * A�� �� ������ ������ ���� �Ȱ��� �� ���� ����
+			 * 두 사람이 S원을 가져야 한다면
 			 * 
-			 * A�� ��� ���� ���� ������ ������ �� �ִ��� Ȯ��
+			 * i 동전의 개수가 C[i]일 때
 			 * 
-			 * i��° ���� ��ȣ�� nums[i], ������ cnts[i]�� �ϸ�
+			 * 각각 0~C[i]만큼 가져갈 수 있음
 			 * 
-			 * dp[k] = N���� ������� k���� ���� �� �ִ���?
+			 * i 동전에서 K만큼 가져갔다면
 			 * 
-			 * 10��,
+			 * 21
 			 * 
+			 * 0
 			 * 
+			 * 0 7
 			 * 
+			 * 0 7 14
 			 * 
+			 * 0 7 14 21
+			 * 
+			 * 0 5 7 12 14 19 21 ---
+			 * 
+			 * 0 5 7 10 12 14 16 19 
+			 * 
+			 * 남은 동전의 합이 목표에 도달하지 못하면 불가능한 경우!
 			 * 
 			 */
+
+			boolean[] checked = new boolean[halfSum+1];
 			
+			checked[0] = true;
 			
+			int lastNum = 0;
+			
+			int[] dp = new int[halfSum+1];
+			for (int j = 0; j < N; j++) {
+				Arrays.fill(dp, 0);
+				int num = coins[j].num;
+				for (int k = 0; k < coins[j].cnt; k++) {
+					
+					
+					
+				}
+			}
+			
+			if(checked[halfSum]) {
+				System.out.println(1);
+			}else {
+				System.out.println(0);
+			}
 		}
 
 	}
--- src/algorithm/baekjoon/Main_2141_우체국.java
+++ src/algorithm/baekjoon/Main_2141_우체국.java
@@ -4,6 +4,15 @@ import java.util.*;
 import java.io.*;
 
 public class Main_2141_우체국{
+	
+	static class Town{
+		long X, A;
+
+		public Town(long x, long a) {
+			X = x;
+			A = a;
+		}
+	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,30 +24,36 @@ public class Main_2141_우체국{
 		 * 
 		 * X[i] 마을에 A[i]명 살음
 		 * 
-		 * 각 사람들까지 거리 합 최소가 되는 위치에 우체국 설치
-		 * 
-		 * 위치 구하기
-		 * 
-		 * 우체국 위치가 P라 하면
-		 * 
-		 * 거리 합 = abs(P - X[i])*A[i]
-		 * 
+		 * 각 사람들까지 거리 합 최소가 되는 위치에 우체국 설치 위치 구하기
 		 */
 		
 		int N = Integer.parseInt(br.readLine());
 		
-		int[] X = new int[N];
-		int[] A = new int[N];
+		Town[] towns = new Town[N];
 		
+		long total = 0;
 		for (int i = 0; i < N; i++) {
 			tokens = new StringTokenizer(br.readLine());
-			X[i] = Integer.parseInt(tokens.nextToken());
-			A[i] = Integer.parseInt(tokens.nextToken());
+			int X = Integer.parseInt(tokens.nextToken());
+			int A = Integer.parseInt(tokens.nextToken());
+			
+			towns[i] = new Town(X,A);
+			total += A;
 		}
 		
-		
-	
-		
+		Arrays.sort(towns,(a,b)->((int)(a.X-b.X)));
+
+		long acc = 0;
+		for (int i = 0; i < N-1; i++) {
+			acc += towns[i].A;
+			long diff = acc + acc - total; // acc - (total - acc);
+
+			if(diff >= 0) {
+				System.out.println(towns[i].X);
+				return;
+			}
+		}
+		System.out.println(towns[N-1].X);
 	}
 
 }
