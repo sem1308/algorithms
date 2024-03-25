950c76a913eef759f523cfb577b7f31a9fa2ea77
sem1308 <43841214+sem1308@users.noreply.github.com>
2024-03-25 17:59:51 +0900

    add code

--- src/algorithm/baekjoon/Main_2631_줄세우기.java
+++ src/algorithm/baekjoon/Main_2631_줄세우기.java
@@ -1,6 +1,5 @@
 package algorithm.baekjoon;
 
-import java.util.*;
 import java.io.*;
 
 public class Main_2631_줄세우기{
@@ -8,20 +7,29 @@ public class Main_2631_줄세우기{
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		StringTokenizer tokens;
-		
 		/*
 		 * 1~N번 아이
+		 * 
+		 * 2 <= N <= 200
 		 * 
 		 * 목적지까지 번호순서대로
 		 * 
 		 * 이동 도중 번호순서가 바뀜
 		 * 
+		 * 옮기는 아이들 수 최소
 		 * 
 		 * 
 		 * 
 		 * 
 		 */
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] nums = new int[N];
+		for (int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(br.readLine());
+		}
+		
 		
 		
 	}
